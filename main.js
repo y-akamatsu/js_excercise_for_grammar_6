98a6073e32b78bde041c3eba8c7f961935a70a0f
y-akamatsu <ak1985tk@gmail.com>
2018-11-25 14:27:19 +0900

    初期化

--- main.js
+++ main.js
@@ -35,44 +35,10 @@
 //           - 第1引数で受けとった `message` の内容を `alert` を使ってアラートダイアログに表示する
 //   - kadai_3関数を実装した直後に「kadai_3(数値, コールバック関数)」を呼び出して、アラートダイアログのメッセージ内容が意図通りであることを確認する
 
-function kadai_3(age, callback) {
-  let message;
-  if (typeof age !== 'number') {
-    message = '数値が入力されていません';
-  } else if (age >= 20) {
-    message = '値は20以上です';
-  } else if (age >= 10) {
-    message = '値は１０以上20未満です';
-  } else {
-    message = '値は10未満です';
-  }
 
-  callback(message);
-}
-kadai_3(22, function(message){
-  alert(message);
-});
-
-kadai_3(15, function(message){
-  alert(message);
-});
-
-kadai_3(9, function(message) {
-  alert(message);
-});
-
-kadai_3('文字列', (message) => {
-  alert(message);
-});
-kadai_3(true, (message) => {
-  alert(message);
-});
 // 課題4: 以下の条件を満たす即時関数を作る
 //   - 2つの引数を受け取る
 //     - 第1引数: x => 数値
 //     - 第2引数: y => 数値
 //   - 処理内容:
 //     - 第1引数のx, 第2引数のyを使って足し算した結果(「x + y」の結果)をconsole.logで出力する。
-(function(x, y){
-  console.log('課題の結果:', x + y);
-})(1, 3)

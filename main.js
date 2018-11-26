3a5ddd8d0613ee4823c638148f3c1acf1de8821b
y-akamatsu <ak1985tk@gmail.com>
2018-11-26 10:45:17 +0900

    復習

--- main.js
+++ main.js
@@ -34,10 +34,11 @@
 //         - 処理内容:
 //           - 第1引数で受けとった `message` の内容を `alert` を使ってアラートダイアログに表示する
 //   - kadai_3関数を実装した直後に「kadai_3(数値, コールバック関数)」を呼び出して、アラートダイアログのメッセージ内容が意図通りであることを確認する
+
 function kadai_3(age, callback) {
   let message;
   if (typeof age !== 'number') {
-    message = '値が入力されていません';
+    message = '値が数値ではありません';
   } else if (age >= 20) {
     message = '値は20以上です';
   } else if (age >= 10) {
@@ -45,64 +46,27 @@ function kadai_3(age, callback) {
   } else {
     message = '値は10未満です';
   }
-
-  callback(message);
-};
+  return callback(message);
+}
 
 kadai_3(25, function (message) {
+  confirm(message);
+});
+kadai_3(15, function (message) {
   alert(message);
 });
 
-kadai_3(19, function (message) {
+kadai_3(5, function (message) {
   alert(message);
 });
-kadai_3(1, function (message) {
-  alert(message);
-});
+
 kadai_3(true, function (message) {
   alert(message);
 });
-kadai_3('25', function (message) {
+
+kadai_3('string', function (message) {
   alert(message);
 });
-
-
-
-// function kadai_3(age, callback){
-//   let message;
-//   if( typeof age !== 'number'){
-//     message = '数値が入力されていません';
-//   } else if (age >= 20){
-//     message = '値は20以上です';
-//   } else if( age >= 10){
-//     message = '値は10以上です';
-//   } else {
-//     message = '値は10未満です';
-//   }
-
-//   callback(message);
-// }
-
-// kadai_3(25, function(message){
-//   alert(message);
-// });
-
-// kadai_3(16, function (message) {
-//   alert(message);
-// });
-
-// kadai_3(8, function (message) {
-//   alert(message);
-// });
-
-// kadai_3(false, function (message) {
-//   alert(message);
-// });
-
-// kadai_3('aaaa', function (message) {
-//   alert(message);
-// });
-
 // 課題4: 以下の条件を満たす即時関数を作る
 //   - 2つの引数を受け取る
 //     - 第1引数: x => 数値
@@ -111,4 +75,4 @@ kadai_3('25', function (message) {
 //     - 第1引数のx, 第2引数のyを使って足し算した結果(「x + y」の結果)をconsole.logで出力する。
 ((x, y) => {
   console.log(x + y);
-})(2,4);
+})(10, 4);
